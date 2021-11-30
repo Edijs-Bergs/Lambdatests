5ff1486514221a00dd291a53226e9072c958d40f
EdijsBergs <86586930+EdijsBergs@users.noreply.github.com>
2021-11-30 09:10:32 +0200

    Files

--- src/test/java/lambdatest/FIT_FI_REGISTERED.java
+++ src/test/java/lambdatest/FIT_FI_REGISTERED.java
@@ -114,7 +114,7 @@ public class FIT_FI_REGISTERED {
 
             driver.findElement(By.xpath("//form[@id='login-form']/button")).click(); //login
 
-            driver.findElement(By.xpath("//img[contains(@src,'https://fitstore.fi/themes/fitstore-fi/assets/img/cart.svg')]")).click(); //Go to cart
+            driver.findElement(By.xpath("/html/body/div[1]/header/div[3]/div/ul/li[4]/a/img")).click(); //Go to cart
             driver.findElement(By.xpath("//div[@id='cartTable']/div[4]/a[2]")).click(); //to checkout
 
             driver.findElement(By.xpath("//div[2]/label")).click(); //Select paysera
