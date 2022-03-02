0d10bf3d8d2f9e3ea1c737422402d3735d0a1f90
EdijsBergs <86586930+EdijsBergs@users.noreply.github.com>
2022-03-02 12:06:46 +0200

    Files

--- src/test/java/Labrains/LAB_REGISTERED.java
+++ src/test/java/Labrains/LAB_REGISTERED.java
@@ -108,7 +108,6 @@ public class LAB_REGISTERED {
             driver.findElement(By.xpath("//*[@id=\"confirm_order_btn\"]")).click(); // Click order btn
             System.out.println("Clicked Next step");
             WebDriverWait wait = new WebDriverWait(driver,30);
-//            driver.findElement(By.xpath("/html/body/div[1]/section[1]/div/div[2]/form/div[6]/div/div/label")).click();
             WebElement element = driver.findElement(By.xpath("//*[@id=\"terms\"]"));
             JavascriptExecutor js =(JavascriptExecutor)driver;
             js.executeScript("arguments[0].click();", element);
