352bc26b06caee251376643e035bd47b4d5e0c9c
EdijsBergs <86586930+EdijsBergs@users.noreply.github.com>
2022-03-03 09:32:29 +0200

    Files

--- src/test/java/Labrains/LAB_REG_LEAGAL.java
+++ src/test/java/Labrains/LAB_REG_LEAGAL.java
@@ -107,27 +107,45 @@ public class LAB_REG_LEAGAL {
             System.out.println("Clicked Next step");
             WebDriverWait wait = new WebDriverWait(driver,40);
 //            driver.findElement(By.xpath("/html/body/div[1]/section[1]/div/div[2]/form/div[6]/div/div/label")).click(); // Accept terms
-            WebElement element = driver.findElement(By.xpath("//*[@id=\"terms\"]"));
-            JavascriptExecutor js =(JavascriptExecutor)driver;
-            js.executeScript("arguments[0].click();", element);
-            System.out.println("Terms Accepted");
-            driver.findElement(By.xpath("//*[@id=\"confirm_order_btn\"]")).click();
-            System.out.println("Purchase created");
+            WebElement GetDelivery = driver.findElement(By.id("shipping_service_label"));
+            String Delivery = GetDelivery.getText();
+            System.out.println(Delivery);
 
-            Thread.sleep(2000);
-            String page_url = driver.getCurrentUrl();
-            String Substring = "popup/pay";
-            boolean result = page_url.contains(Substring);
-            System.out.println(result);
-            if (result)
+            String NotClick = "DPD Pickup point";
+            boolean DeliverMethod = Delivery.contains(NotClick);
+            System.out.println(DeliverMethod);
+            if (DeliverMethod)
             {
-                Resulting = "passed";
+                WebElement element = driver.findElement(By.xpath("//*[@id=\"terms\"]"));
+                JavascriptExecutor js =(JavascriptExecutor)driver;
+                js.executeScript("arguments[0].click();", element);
+                System.out.println("Terms Accepted");
+                driver.findElement(By.xpath("//*[@id=\"confirm_order_btn\"]")).click();
+                System.out.println("Purchase created");
+
+                Thread.sleep(2000);
+                String page_url = driver.getCurrentUrl();
+                System.out.println(page_url);
+
+                String Substring = "popup/pay";
+                boolean result = page_url.contains(Substring);
+                System.out.println(result);
+                if (result)
+                {
+                    Resulting = "passed";
+                }
+                else
+                {
+                    Resulting = "failed";
+                }
+                System.out.println(Resulting);
             }
             else
             {
                 Resulting = "failed";
+                System.out.println("Wasnt  pickup in store");
+
             }
-            System.out.println(Resulting);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
